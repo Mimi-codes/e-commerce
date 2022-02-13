38d5668a5f00047be9e375e0e205d1eb06a04154
Tomi <mariamismael904@gmail.com>
2022-02-13 23:43:48 +0100

    added some files

--- src/components/Layout/Testimonial/ProgressBar.js
+++ src/components/Layout/Testimonial/ProgressBar.js
@@ -3,8 +3,10 @@ import classes from './ProgressBar.module.css';
 function ProgressBar() {
 return (
     <>
+    <div className={classes['progress-bar']}>
     <label for='file'>1<subscript>/10</subscript></label>
-    <progress id='file' value= '1' max='100'>1</progress>
+    <progress id='file' value= '20' max='100'>1</progress>
+</div>
 </>
 )
 }
